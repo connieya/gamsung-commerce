e3d7a9997627acff0bf9f6282b13721c71054ec3
connieya <gunny6026@naver.com>
2025-07-13 22:26:22 +0900

    build: Add Spring Boot Starter Validation dependency

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${project.properties["springDocOpenApiVersion"]}")
+    implementation("org.springframework.boot:spring-boot-starter-validation:3.5.0")
 
     // querydsl
     implementation("com.querydsl:querydsl-jpa::jakarta")

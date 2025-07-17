a69a64f89d96b33a312a4100ebf5a6026ebe38aa
connieya <gunny6026@naver.com>
2025-07-18 00:17:31 +0900

    test : 회원가입 통합 테스트

---
+++ apps/commerce-api/src/test/java/com/loopers/application/user/UserServiceIntegrationTest.java
@@ -0,0 +1,73 @@
+package com.loopers.application.user;
+
+import com.loopers.application.user.exception.UserException;
+import com.loopers.application.user.port.in.UserRegisterCommand;
+import com.loopers.application.user.port.out.UserRepositoryOut;
+import com.loopers.domain.user.User;
+import com.loopers.domain.user.vo.Gender;
+import com.loopers.utils.DatabaseCleanUp;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserServiceIntegrationTest {
+
+    @Autowired
+    UserService userService;
+
+    @MockitoSpyBean
+    UserRepositoryOut userRepositoryOut;
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanUp;
+
+    @AfterEach
+    void tearDown() {
+        databaseCleanUp.truncateAllTables();
+    }
+
+    @DisplayName("회원 가입")
+    @Nested
+    class Register {
+
+        @DisplayName("이미 가입된 ID 로 회원가입 시도 시, 실패한다.")
+        @Test
+        void registerFailWhenUserAlreadyExists() {
+            // given
+            String userId = "testUser";
+
+            doReturn(Optional.of(User.create(userId, "dd@naver.com", "1999-09-09", Gender.FEMALE))).when(userRepositoryOut).findByUserId(userId);
+
+            // when , then
+            assertThatThrownBy(() -> {
+                userService.register(UserRegisterCommand.of(userId, "geonhee@naver.com", "1994-09-26", Gender.MALE));
+            }).isInstanceOf(UserException.UserAlreadyExistsException.class);
+
+        }
+
+        @DisplayName("회원 가입시 User 저장이 수행된다.")
+        @Test
+        void registerSuccess() {
+            // given
+            String userId = "testUser";
+
+            // when
+            userService.register(UserRegisterCommand.of(userId, "geonhee@naver.com", "1994-09-26", Gender.MALE));
+
+            // then
+            verify(userRepositoryOut, times(1)).save(any(User.class));
+
+        }
+    }
+
+}

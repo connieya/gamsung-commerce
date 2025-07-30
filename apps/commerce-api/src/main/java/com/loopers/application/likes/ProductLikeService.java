59074a2d0191ad7df942bf511dd5fa6b88a7744d
connieya <gunny6026@naver.com>
2025-07-30 23:39:58 +0900

    test : 좋아요 취소 단위 테스트

--- apps/commerce-api/src/main/java/com/loopers/application/likes/ProductLikeService.java
+++ apps/commerce-api/src/main/java/com/loopers/application/likes/ProductLikeService.java
@@ -3,6 +3,7 @@ package com.loopers.application.likes;
 import com.loopers.domain.likes.ProductLikeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,15 +11,17 @@ public class ProductLikeService {
 
     private final ProductLikeRepository productLikeRepository;
 
+    @Transactional
     public void add(Long userId, Long productId) {
         boolean existed = productLikeRepository.existsByUserIdAndProductId(userId, productId);
 
         if (!existed) {
-            productLikeRepository.save(userId,productId);
+            productLikeRepository.save(userId, productId);
         }
     }
 
+    @Transactional
     public void remove(Long userId, Long productId) {
-
+        productLikeRepository.delete(userId, productId);
     }
 }
--- apps/commerce-api/src/test/java/com/loopers/application/likes/ProductLikeServiceTest.java
+++ apps/commerce-api/src/test/java/com/loopers/application/likes/ProductLikeServiceTest.java
@@ -38,7 +38,7 @@ class ProductLikeServiceTest {
 
 
     @Test
-    @DisplayName("좋아요: 중복 요청 시에도 한 번만 저장되어 멱등성이 보장된다.")
+    @DisplayName("좋아요: 이미 등록된 경우, 중복 저장되지 않아 멱등성이 보장된다.")
     void add_ensuresIdempotency_onDuplicate() {
         // given
         Long productId = 1L;
@@ -52,6 +52,20 @@ class ProductLikeServiceTest {
 
         // then
         verify(productLikeRepository, never()).save(userId, productId);
+    }
+
+    @Test
+    @DisplayName("좋아요: 요청 시 성공적으로 삭제된다.")
+    void remove_successfullyDeletesLike() {
+        // given
+        Long userId = 1L;
+        Long productId = 1L;
+
+        // when
+        sut.remove(userId, productId);
+
+        // then.
+        verify(productLikeRepository, times(1)).delete(userId, productId);
     }
 
 }

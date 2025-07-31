925c7b678dab3a981e347848e805b2cdce581f66
connieya <gunny6026@naver.com>
2025-08-01 01:58:13 +0900

    feat : 주문 파사드 생성

--- apps/commerce-api/src/main/java/com/loopers/application/order/OrderFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/order/OrderFacade.java
@@ -1,9 +1,17 @@
 package com.loopers.application.order;
 
+import com.loopers.domain.order.Order;
+import com.loopers.domain.order.OrderCommand;
+import com.loopers.domain.order.OrderService;
+import com.loopers.domain.point.PointService;
 import com.loopers.domain.product.Product;
 import com.loopers.domain.product.ProductService;
+import com.loopers.domain.product.stock.StockService;
+import com.loopers.domain.user.User;
+import com.loopers.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,9 +19,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderFacade {
 
-    ProductService productService;
+    private final ProductService productService;
+    private final UserService userService;
+    private final OrderService orderService;
+    private final PointService pointService;
+    private final StockService stockService;
 
+    @Transactional
     public void place(OrderCriteria orderCriteria) {
+        User user = userService.findByUserId(orderCriteria.getUserId());
         List<Product> products = productService.findAllById(orderCriteria.getProductIds());
+        OrderCommand command = orderCriteria.toCommand(products, user.getId());
+        Order order = orderService.place(command);
+        pointService.deduct(user.getUserId(), order.getTotalAmount());
+        stockService.deduct(orderCriteria.getProductIds(), command);
     }
 }

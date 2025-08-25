56b6d6dc801d0b4b167e869120f0e478b929a205
connieya <gunny6026@naver.com>
2025-08-25 11:13:43 +0900

    feat : PG 결제 요청 API fallback 메서드 시그니처 변경

--- apps/commerce-api/src/test/java/com/loopers/infrastructure/payment/client/PgSimulatorTest.java
+++ apps/commerce-api/src/test/java/com/loopers/infrastructure/payment/client/PgSimulatorTest.java
@@ -1,33 +1,22 @@
 package com.loopers.infrastructure.payment.client;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.loopers.domain.payment.*;
 import com.loopers.domain.payment.exception.PaymentException;
-import com.loopers.interfaces.api.ApiHeaders;
-import com.loopers.interfaces.api.payment.PaymentV1Dto;
 import feign.Request;
-import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -40,8 +29,6 @@ class PgSimulatorTest {
     @Autowired
     private PgSimulator pgSimulator;
 
-    @MockitoBean
-    private PaymentRepository paymentRepository;
 
     @BeforeEach
     void setUp() {
@@ -59,12 +46,7 @@ class PgSimulatorTest {
                         .withFixedDelay(4000)
                 ));
 
-
         PaymentCommand.Transaction transaction = PaymentCommand.Transaction.of("12345", 1L, CardType.HYUNDAI, "1234-5678-9012-3456", 1000L);
-
-        Payment mockPayment = mock(Payment.class);
-        when(paymentRepository.findById(1L))
-                .thenReturn(Optional.of(mockPayment));
 
 
         for (int i = 0; i < REQUIRED_FAILURES_TO_OPEN_CIRCUIT; i++) {

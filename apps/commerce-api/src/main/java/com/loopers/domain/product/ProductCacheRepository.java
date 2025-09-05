1f78d4e2f73221fd8495497c2a852fdc76a20d2f
connieya <gunny6026@naver.com>
2025-09-05 13:26:29 +0900

    feat: 상품 상세 조회 캐시 로직 수정

--- apps/commerce-api/src/main/java/com/loopers/domain/product/ProductCacheRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductCacheRepository.java
@@ -3,9 +3,15 @@ package com.loopers.domain.product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 public interface ProductCacheRepository {
 
     Page<ProductInfo> findProductDetails(Pageable pageable , Long brandId);
 
     void save(Long brandId, Page<ProductInfo> productInfoPage);
+
+    Optional<ProductDetailInfo> findProductDetailById(Long productId);
+
+    void saveProductDetail(Long productId, ProductDetailInfo productDetailInfo);
 }
--- apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductCacheRepositoryImpl.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductCacheRepositoryImpl.java
@@ -3,23 +3,30 @@ package com.loopers.infrastructure.product;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.loopers.config.RedisCacheConfig;
 import com.loopers.domain.product.ProductCacheRepository;
+import com.loopers.domain.product.ProductDetailInfo;
 import com.loopers.domain.product.ProductInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class ProductCacheRepositoryImpl implements ProductCacheRepository {
 
+    private final RedisTemplate<String, Object> objectRedisTemplate;
     private final StringRedisTemplate stringRedisTemplate;
     private final ObjectMapper objectMapper;
 
@@ -60,6 +67,44 @@ public class ProductCacheRepositoryImpl implements ProductCacheRepository {
             throw new RuntimeException(e);
         }
         stringRedisTemplate.opsForValue().set(key, json, TTL);
+    }
+
+    @Override
+    public Optional<ProductDetailInfo> findProductDetailById(Long productId) {
+        String key = "product.detail:" +productId;
+        Map<String, Object> cache = objectRedisTemplate.<String, Object>opsForHash().entries(key);
+
+        if (CollectionUtils.isEmpty(cache)) {
+            return Optional.empty();
+        }
+
+
+        if (cache.containsKey("__null__")) {
+            return Optional.of(ProductDetailInfo.EMPTY);
+        }
+        ProductDetailInfo productDetailInfo = objectMapper.convertValue(cache, new TypeReference<>() {
+        });
+        return Optional.of(productDetailInfo);
+    }
+
+    @Override
+    public void saveProductDetail(Long productId, ProductDetailInfo productDetailInfo) {
+        String key = "product.detail:" + productId;
+        Duration ttl = RedisCacheConfig.jitter(Duration.ofMinutes(3));
+
+
+        // 캐시 관통을 방지한다.
+        if (productDetailInfo == null) {
+            objectRedisTemplate.opsForHash().putAll(key, Map.of("__null__", "null"));
+            objectRedisTemplate.expire(key, ttl);
+            return;
+        }
+
+
+        Map<String, Object> cache = objectMapper.convertValue(productDetailInfo, new TypeReference<>() {
+        });
+        objectRedisTemplate.opsForHash().putAll(key, cache);
+        objectRedisTemplate.expire(key, ttl);
     }
 
 

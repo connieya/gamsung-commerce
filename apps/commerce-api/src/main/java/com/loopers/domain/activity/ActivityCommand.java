ca7ee4eb96ddf27fc5295415038eb2ab2707c3f8
connieya <gunny6026@naver.com>
2025-09-06 16:04:01 +0900

    feat: 상품 조회 수 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/activity/ActivityCommand.java
@@ -0,0 +1,11 @@
+package com.loopers.domain.activity;
+
+import lombok.Getter;
+
+public class ActivityCommand {
+
+    @Getter
+    public static class View {
+        private Long productId;
+    }
+}

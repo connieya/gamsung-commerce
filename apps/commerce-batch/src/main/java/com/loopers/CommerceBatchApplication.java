12e860625dacb9943803eadf0f942052148e476c
connieya <gunny6026@naver.com>
2025-09-18 00:00:29 +0900

    feat: 배치 모듈 생성

---
+++ apps/commerce-batch/src/main/java/com/loopers/CommerceBatchApplication.java
@@ -0,0 +1,20 @@
+package com.loopers;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class CommerceBatchApplication {
+    @PostConstruct
+    public void started() {
+        // set timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceBatchApplication.class, args);
+    }
+}
